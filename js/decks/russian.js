333b05e772b308c99120f022047c7acc040015fc
Keith Roshangar <roshangk@users.noreply.github.com>
2016-02-07 14:44:11 -0800

    Create russian.js

---
+++ js/decks/russian.js
@@ -0,0 +1,38 @@
+decks.push(
+{
+    name: 'Russian alphabet',
+    description: 'Learn the Russian alphabet',
+    cards: [
+         {source: 'А', target: 'a'},
+         {source: 'Б', target: 'b'},
+         {source: 'В', target: 'v'},
+         {source: 'Г', target: 'g'},
+         {source: 'Д', target: 'd'},
+         {source: 'Е', target: 'ye'},
+         {source: 'Ё', target: 'yo'},
+         {source: 'Ж', target: 'zh'},
+         {source: 'З', target: 'z'},
+         {source: 'И', target: 'i'},
+         {source: 'Й', target: 'y'},
+         {source: 'К', target: 'k'},
+         {source: 'Л', target: 'l'},
+         {source: 'М', target: 'm'},
+         {source: 'Н', target: 'n'},
+         {source: 'О', target: 'o'},
+         {source: 'П', target: 'p'},
+         {source: 'Р', target: 'r'},
+         {source: 'С', target: 's'},
+         {source: 'Т', target: 't'},
+         {source: 'У', target: 'u'},
+         {source: 'Ф', target: 'f'},
+         {source: 'Х', target: 'kh'},
+         {source: 'Ц', target: 'ts'},
+         {source: 'Ч', target: 'ch'},
+         {source: 'Ш', target: 'sh'},
+         {source: 'Щ', target: 'shch'},
+         {source: 'Ы', target: 'i'},
+         {source: 'Э', target: 'e'},
+         {source: 'Ю', target: 'yu'},
+         {source: 'Я', target: 'ya'}
+    ]
+});
